ce6e89f2a64c1aa06eb72ece863e0c001e3e0b4d
Kavipriya Ramanathan <kavipriya2601@outlook.com>
2026-04-03 21:32:52 +0530

    import makestyle

--- client/src/componets/Blogs.js
+++ client/src/componets/Blogs.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { makeStyles } from "@mui/styles";
 import Blog from "./Blog";
 import config from "../config";
 import EmptyBlog from "./EmptyBlog";
